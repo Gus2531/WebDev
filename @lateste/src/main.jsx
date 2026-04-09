efd4515b320d05d053229948adddd7538a0c28a6
0-GK-0 <gkenzo11@gmail.com>
2026-04-09 15:26:46 -0300

    criei aula10

--- @lateste/src/main.jsx
+++ @lateste/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import reactDom from "react-dom/client"
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+reactDom.createRoot(document.getElementById("root")).render( <App /> )
